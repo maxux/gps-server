bdac7d1bb8b1a81874aa86fae117c7da948a27df
Maxime Daniel <root@maxux.net>
2017-09-26 00:10:42 +0200

    Add sample config file

---
+++ config/gpsconf.sample.py
@@ -0,0 +1,29 @@
+# gps-server global configuration
+config = {
+    # sqlite database file
+    'db-file': "db/gps.sqlite3",
+
+    # http post (session, datapoint) password
+    'password': '',
+
+    # public url for reaching pages
+    'public-url': 'gps.domain.tld',
+
+    # public url for reaching live websocket
+    'public-live-url': 'live.gps.domain.tld',
+
+    # http listen port
+    'http-port': 5555,
+
+    # live websocket port
+    'websocket-port': 5556,
+
+    # http listen address
+    'http-listen': '0.0.0.0',
+
+    # websocket listen address
+    'websocket-listen': '0.0.0.0',
+
+    # enable flask debug
+    'debug': True,
+}
